364a718f224e827a7dc7693f5ad1a4d3e5f29497
mkatla <mukeshchandrakatla@gmail.com>
2019-09-04 22:55:48 -0500

    added two new resources for dao methods.

--- src/main/java/com/mukesh/practise/controller/UsersController.java
+++ src/main/java/com/mukesh/practise/controller/UsersController.java
@@ -1,8 +1,10 @@
 package com.mukesh.practise.controller;
 
+import com.mukesh.practise.dao.UsersDaoInterface;
 import com.mukesh.practise.model.UsersModel;
 import com.mukesh.practise.service.UsersService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfigureOrder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,14 +14,23 @@ import java.util.List;
 public class UsersController {
     @Autowired
     private UsersService usersService;
+    @Autowired
+    private UsersDaoInterface usersDaoInterface;
     @GetMapping(value="/users")
     //@ResponseBody
     public List<UsersModel> getUsersList(){
      return usersService.getUsers();
     }
 
-    @PostMapping(value = "/userDetails")
+    @GetMapping(value = "/userDetails")
     public List<UsersModel> postUserDetails(){
         return usersService.postUserDetails();
+    }
+    @GetMapping(value = "/all")
+    public List<UsersModel> getAll(){return usersDaoInterface.findAll();}
+    @PostMapping(value = "/loadAll")
+    public List<UsersModel> persist(@RequestBody final UsersModel users){
+        usersDaoInterface.save(users);
+        return  usersDaoInterface.findAll();
     }
 }
